ac25a13074d805e05db102c9c0efb3a8608fee97
nodes777 <nodes777@gmail.com>
2024-04-17 10:09:24 +1000

    Batches update

--- src/components/Rain/Rain.tsx
+++ src/components/Rain/Rain.tsx
@@ -1,65 +1,68 @@
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
 
 import styles from "./styles.module.css";
 import { RainDrop } from "./Raindrop";
 
+const generateRainDrops = () => {
+  const newRainDrops = [];
+  for (let i = 0; i < 1000; i++) {
+    const left = Math.random() * 2 * window.innerWidth;
+    const top = Math.random() * window.innerHeight;
+    const rate = Math.random() * 4 + 1;
+
+    // Create raindrop object with initial state
+    newRainDrops.push({ top, left, rate });
+  }
+  return newRainDrops;
+};
+
 export const Rain = () => {
   const [rainDrops, setRainDrops] = useState<
     { top: number; left: number; rate: number }[]
-  >([]);
+  >(generateRainDrops());
 
-  const generateRainDrops = () => {
-    const newRainDrops = [];
-    for (let i = 0; i < 1000; i++) {
-      const left = Math.random() * 2 * window.innerWidth;
-      const top = Math.random() * window.innerHeight;
-      const rate = Math.random() * 4 + 1;
-
-      // Create raindrop object with initial state
-      newRainDrops.push({ top, left, rate });
-    }
-    return newRainDrops;
-  };
+  const snowRef = useRef<number>();
 
   useEffect(() => {
-    const rainDrops = generateRainDrops();
-    setRainDrops(rainDrops);
     document.body.style.overflow = "hidden"; // Hide scrollbar hack
-    return () => {
-      document.body.style.overflow = "auto";
-    };
+    snowRef.current = requestAnimationFrame(step);
+    return () => cancelAnimationFrame(snowRef.current ?? 0);
   }, []);
 
-  // const step = () => {
-  //   rainDrops.forEach((rainDrop) => {
-  //     const { top, left, rate } = rainDrop; // Destructure directly
-  //     if (top > window.innerHeight || left < 0) {
-  //       // Update state using spread operator
-  //       setRainDrops((prevRainDrops) =>
-  //         prevRainDrops.map((r) =>
-  //           r === rainDrop
-  //             ? { top: 0, left: Math.random() * 2 * window.innerWidth, rate }
-  //             : r
-  //         )
-  //       );
-  //     } else {
-  //       // Update state using spread operator
-  //       setRainDrops((prevRainDrops) =>
-  //         prevRainDrops.map((r) =>
-  //           r === rainDrop ? { top: top + rate, left: left - rate, rate } : r
-  //         )
-  //       );
-  //     }
-  //   });
-  //   requestAnimationFrame(step);
-  // };
+  const step = () => {
+    // const newDrops = rainDrops.map((rainDrop) => {
+    //   const { top, left, rate } = rainDrop;
+    //   if (top > window.innerHeight || left < 0) {
+    //     return { top: 0, left: Math.random() * 2 * window.innerWidth, rate };
+    //   } else {
+    //     return { top: top + rate, left: left - rate, rate };
+    //   }
+    // });
+    setRainDrops((prevRainDrops) =>
+      prevRainDrops.map((rainDrop) => {
+        const { top, left, rate } = rainDrop;
+        if (top > window.innerHeight || left < 0) {
+          return { top: 0, left: Math.random() * 2 * window.innerWidth, rate };
+        } else {
+          return { top: top + rate, left: left - rate, rate };
+        }
+      })
+    );
 
-  // requestAnimationFrame(step);
+    snowRef.current = requestAnimationFrame(step);
+  };
 
   return (
     <div className={styles.rainContainer}>
       {rainDrops.map((rainDrop, i) => (
-        <RainDrop key={i} drop={rainDrop} />
+        <div
+          key={i}
+          className={styles.rainDrop}
+          style={{
+            left: `${rainDrop.left}px`,
+            top: `${rainDrop.top}px`,
+          }}
+        />
       ))}
     </div>
   );
